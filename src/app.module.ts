81bc3b30cfe7b1bdfbe03feaf2d23c83da14e6ac
Angel Antonio Barco Alfaro <angel.barco@kavak.com>
2022-07-11 00:04:11 -0500

    feat: add postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppsModule } from './apps/apps.module';
+import { createTypeOrmOptions } from './utils/typeorm-config/typeorm-config.service';
 
 @Module({
-  imports: [AppsModule],
+  imports: [
+    AppsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: createTypeOrmOptions,
+      inject: [ConfigService],
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
---
+++ src/db/typeorm-config/typeorm-config.service.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const createTypeOrmOptions = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [],
+    synchronize: true,
+  };
+};
