6594f644db58b7d57b592b2dd8b76a78aa814717
Angel Antonio Barco Alfaro <angel.barco@kavak.com>
2022-07-11 19:44:50 -0500

    feat: create requests for Job

--- src/apps/job/job.module.ts
+++ src/apps/job/job.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RepositoryModule } from 'src/db/postgres/repository';
 import { JobController } from './job.controller';
+import { JobService } from './services/job/job.service';
 
 @Module({
+  imports: [RepositoryModule],
   controllers: [JobController],
+  providers: [JobService],
 })
 export class JobModule {}
