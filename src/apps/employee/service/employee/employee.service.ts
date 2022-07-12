eb8c14c5634219f4704b3a97f350f664d3dc186b
Angel Antonio Barco Alfaro <angel.barco@kavak.com>
2022-07-11 23:40:20 -0500

    feat: create employee service

--- src/apps/employee/service/employee/employee.service.ts
+++ src/apps/employee/service/employee/employee.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Employee } from 'src/db/postgres/entities';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class EmployeeService {
-  getAllEmployees() {
-    return [];
+  constructor(
+    @InjectRepository(Employee)
+    private employeeRepository: Repository<Employee>,
+  ) {}
+
+  getAllEmployees(): Promise<Employee[]> {
+    return this.employeeRepository.find();
+  }
+
+  getEmployeeById(id: number): Promise<Employee> {
+    return this.employeeRepository.findOneBy({ id });
+  }
+
+  createEmployee(
+    employeeData: Omit<Partial<Employee>, 'id'>,
+  ): Promise<Employee> {
+    return this.employeeRepository.save(employeeData);
+  }
+
+  async updateEmployee(
+    id: number,
+    newEmployeeData: Omit<Partial<Employee>, 'id'>,
+  ): Promise<Employee> {
+    const previousEmployeeData = await this.employeeRepository.findOneBy({
+      id,
+    });
+
+    if (!previousEmployeeData)
+      throw new NotFoundException("Employee doesn't exist.");
+
+    const updatedEmployeeData = Object.assign(
+      previousEmployeeData,
+      newEmployeeData,
+    );
+
+    return this.employeeRepository.save(updatedEmployeeData);
+  }
+
+  deleteEmployee(id: number): Promise<DeleteResult> {
+    return this.employeeRepository.softDelete(id);
   }
 }
